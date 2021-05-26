1e0b702be4ca612b2d005689b81ed36b275e5bcb
Danner Uribe <Danner Uribe@host.docker.internal>
2021-05-25 23:55:32 -0500

    Se versionan los ajustes realizados durante la sesión 13.

--- workspace/restaurante-semillero-usuarios/src/main/java/com/heinsohn/semillero/entity/Usuarios.java
+++ workspace/restaurante-semillero-usuarios/src/main/java/com/heinsohn/semillero/entity/Usuarios.java
@@ -7,6 +7,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -42,7 +43,7 @@ public class Usuarios {
 	@Size(max = 150, min = 10)
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 	  name = "USURIO_ROL", 
 	  joinColumns = @JoinColumn(name = "ID_USUARIO"), 
